f338d1b94657ea1a7ca8d3d7d9a339b61a189ace
jaypacamarra <jpacamar97@gmail.com>
2021-05-24 23:51:49 -0700

    Add Serial_Receive_DMA function to receive serial data via DMA

--- dsx-fw/DSX_Library/Serial/Inc/serial.h
+++ dsx-fw/DSX_Library/Serial/Inc/serial.h
@@ -8,8 +8,14 @@
 #ifndef SERIAL_INC_SERIAL_H_
 #define SERIAL_INC_SERIAL_H_
 
+#include "main.h"
 
 
+// HLPUART handle
+extern UART_HandleTypeDef hlpuart1;
+
+// Function to receive serial data via DMA
+void Serial_Receive_DMA(void);
 
 
 #endif /* SERIAL_INC_SERIAL_H_ */
--- dsx-fw/DSX_Library/Serial/Src/serial.c
+++ dsx-fw/DSX_Library/Serial/Src/serial.c
@@ -6,3 +6,10 @@
  */
 
 #include "serial.h"
+
+static uint8_t buffer[12];
+
+void Serial_Receive_DMA(void)
+{
+	HAL_UART_Receive_DMA(&hlpuart1, buffer, sizeof(buffer));
+}
