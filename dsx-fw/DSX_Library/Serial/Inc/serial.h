ebbdbe828923da54f7d368fafba169f031672b3d
jaypacamarra <jpacamar97@gmail.com>
2021-05-24 17:36:28 -0700

    Add empty Serial driver

---
+++ dsx-fw/DSX_Library/Serial/Inc/serial.h
@@ -0,0 +1,15 @@
+/*
+ * serial.h
+ *
+ *  Created on: May 24, 2021
+ *      Author: jpaca
+ */
+
+#ifndef SERIAL_INC_SERIAL_H_
+#define SERIAL_INC_SERIAL_H_
+
+
+
+
+
+#endif /* SERIAL_INC_SERIAL_H_ */
---
+++ dsx-fw/DSX_Library/Serial/Src/serial.c
@@ -0,0 +1,8 @@
+/*
+ * serial.c
+ *
+ *  Created on: May 24, 2021
+ *      Author: jpaca
+ */
+
+#include "serial.h"
