6e582b801d55b658443e63e7793200bebcd7712f
jaypacamarra <jpacamar97@gmail.com>
2021-05-25 21:47:23 -0700

    Update uart rx callback and add serial send
    The UART Rx callback now converts ascii elements in the buffer to
    integers. I also added a Serial_Transmit_DMA() function for sending
    serial data back to simulink like what we did in the prototype fw.

--- dsx-fw/DSX_Library/Serial/Inc/serial.h
+++ dsx-fw/DSX_Library/Serial/Inc/serial.h
@@ -19,8 +19,8 @@ extern UART_HandleTypeDef hlpuart1;
 // Function to receive serial data via DMA
 void Serial_Receive_DMA(void);
 
-// Function to convert ascii elements in buffer to integer
-void buffer_ascii_2_int(void);
+// Function to transmit serial data via DMA
+void Serial_Transmit_DMA(volatile DSX_data_t *data);
 
 // Function to parse buffer and store into dsx data
 void parse_buffer_to_dsx_data(volatile DSX_data_t *data);
--- dsx-fw/DSX_Library/Serial/Src/serial.c
+++ dsx-fw/DSX_Library/Serial/Src/serial.c
@@ -9,44 +9,54 @@
 
 #define BUFFER_SIZE		11U
 
-static uint8_t buffer[BUFFER_SIZE] = {0};
+static uint8_t rx_buffer[BUFFER_SIZE] = {0};
+static uint8_t tx_buffer[BUFFER_SIZE] = {0};
 
 // Function to receive serial data via DMA
 void Serial_Receive_DMA(void)
 {
-	HAL_UART_Receive_DMA(&hlpuart1, buffer, sizeof(buffer));
+	HAL_UART_Receive_DMA(&hlpuart1, rx_buffer, sizeof(rx_buffer));
 }
 
-// Function to convert ascii elements in buffer to integer
-void buffer_ascii_2_int(void)
+// Function to transmit serial data via DMA
+void Serial_Transmit_DMA(volatile DSX_data_t *data)
 {
-	for (int i=0 ; i<BUFFER_SIZE-1 ; i++){
-
-		//if buffer element is a digit (0-9) (See ASCII Table)
-		if(buffer[i] >= 48 && buffer[i] <= 54)
-		{
-			buffer[i] = buffer[i] - '0';
-		}
-	}
+	tx_buffer[0] = data->ID / 10;
+	tx_buffer[1] = data->ID % 10;
+	tx_buffer[2] = data->loc / 10;
+	tx_buffer[3] = data->loc % 10;
+	tx_buffer[4] = data->sign;
+	tx_buffer[5] = data->val / 1000;
+	tx_buffer[6] = (data->val / 100) % 10;
+	tx_buffer[7] = (data->val / 10) % 10;
+	tx_buffer[8] = data->val % 10;
+	tx_buffer[9] = data->val / 10000;
+	tx_buffer[10] = '\n';
 }
 
 // Function to parse buffer and store into dsx data
 void parse_buffer_to_dsx_data(volatile DSX_data_t *data)
 {
-	buffer_ascii_2_int();
-
-	if(buffer[10] == '\n')
+	if(rx_buffer[10] == '\n')
 	{
-		data->ID = buffer[0]*10 + buffer[1];
-		data->loc = buffer[2]*10 + buffer[3];
-		data->sign = buffer[4];
-		data->val = buffer[5]*1000 + buffer[6]*100 + buffer[7]*10 + buffer[8];
-		data->ret = buffer[9];
+		data->ID 	= rx_buffer[0]*10 + rx_buffer[1];
+		data->loc 	= rx_buffer[2]*10 + rx_buffer[3];
+		data->sign 	= rx_buffer[4];
+		data->val 	= rx_buffer[5]*1000 + rx_buffer[6]*100 + rx_buffer[7]*10 + rx_buffer[8];
+		data->ret 	= rx_buffer[9];
 	}
 }
 
 // Callback for LPUART1
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-	// Do we need to do anything when the UART callback is called?
+	// convert ascii elements in buffer to integer
+	for (int i=0 ; i<BUFFER_SIZE-1 ; i++)
+	{
+		//if buffer element is a digit (0-9) (See ASCII Table)
+		if(rx_buffer[i] >= 48 && rx_buffer[i] <= 57)
+		{
+			rx_buffer[i] = rx_buffer[i] - '0';
+		}
+	}
 }
