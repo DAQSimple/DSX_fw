e5a29ea6b702f5f1a9a9d61cfd732556a32b04dd
jaypacamarra <jpacamar97@gmail.com>
2021-05-30 00:44:45 -0700

    Create new type for channel to split port and pin

---
+++ dsx-fw/DSX_Library/Command/Src/channel.c
@@ -0,0 +1,16 @@
+/*
+ * channel.c
+ *
+ *  Created on: May 30, 2021
+ *      Author: jpaca
+ */
+
+#include "channel.h"
+
+// Function to convert loc to channel_t
+// check loc and compare against pin defines in board_defines.h
+// Assign appropriate port and pin
+channel_t convert_loc_to_channel(uint32_t loc)
+{
+
+}
