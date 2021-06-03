b74fe23040c54c90b5ec704e7864b18f0edbc61a
jaypacamarra <jpacamar97@gmail.com>
2021-06-03 13:46:33 -0700

    create empty safety driver

---
+++ dsx-fw/DSX_Library/Safety/Inc/safety.h
@@ -0,0 +1,13 @@
+/*
+ * safety.h
+ *
+ *  Created on: Jun 3, 2021
+ *      Author: jpaca
+ */
+
+#ifndef SAFETY_H
+#define SAFETY_H
+
+
+
+#endif /* SAFETY_H */
---
+++ dsx-fw/DSX_Library/Safety/Src/safety.c
@@ -0,0 +1,8 @@
+/*
+ * safety.c
+ *
+ *  Created on: Jun 3, 2021
+ *      Author: jpaca
+ */
+
+#include "safety.h"
