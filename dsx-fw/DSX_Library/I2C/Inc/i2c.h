2958047afb7843db12a42cb6bf09116490a75afd
jaypacamarra <jpacamar97@gmail.com>
2021-06-24 01:54:31 -0700

    create empty i2c driver

---
+++ dsx-fw/DSX_Library/I2C/Inc/i2c.h
@@ -0,0 +1,17 @@
+/*
+ * i2c.h
+ *
+ *  Created on: Jun 24, 2021
+ *      Author: jpaca
+ */
+
+#ifndef I2C_H
+#define I2C_H
+
+#include <stdint.h>
+#include "main.h"
+
+// driver function for i2c write
+// void I2C_Write(uint8_t slave_address, uint16_t value);
+
+#endif /* I2C_H */
---
+++ dsx-fw/DSX_Library/I2C/Src/i2c.c
@@ -0,0 +1,8 @@
+/*
+ * i2c.c
+ *
+ *  Created on: Jun 24, 2021
+ *      Author: jpaca
+ */
+#include "i2c.h"
+
