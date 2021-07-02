e52a329a78e3111242c2061e98d10f78e5f5f8a5
manraj-s <33184179+manraj-s@users.noreply.github.com>
2021-07-02 16:23:55 -0700

    added SPI folder, updated include paths

---
+++ dsx-fw/DSX_Library/SPI/Inc/SPI.h
@@ -0,0 +1,13 @@
+/*
+ * SPI.h
+ *
+ *  Created on: Jul. 2, 2021
+ *      Author: Manraj Sangha
+ */
+
+#ifndef SPI_INC_SPI_H_
+#define SPI_INC_SPI_H_
+
+
+
+#endif /* SPI_INC_SPI_H_ */
---
+++ dsx-fw/DSX_Library/SPI/Src/SPI.c
@@ -0,0 +1,9 @@
+/*
+ * SPI.c
+ *
+ *  Created on: Jul. 2, 2021
+ *      Author: Manraj Sangha
+ */
+
+#include "SPI.h"
+
