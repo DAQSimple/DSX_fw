8c47aa918620fe12fbaabc92fc079a3936b22983
Harjaapb <hsbharjaap@gmail.com>
2021-07-08 15:23:40 -0700

    Update encoder validation

--- dsx-fw/DSX_Library/Command/Src/validate.c
+++ dsx-fw/DSX_Library/Command/Src/validate.c
@@ -79,7 +79,10 @@ bool validate_servo_write(volatile DSX_data_t *dsx_data){
 }
 
 // Validate Read Encoder Speed and Direction Command
-bool validate_read_encoder(volatile DSX_data_t *dsx_data){};
+bool validate_read_encoder(volatile DSX_data_t *dsx_data){
+	bool check_data = true;
+	return check_data;
+}
 
 // Validate Get Serial Info Command
 bool validate_get_serial_info(volatile DSX_data_t *dsx_data){};
